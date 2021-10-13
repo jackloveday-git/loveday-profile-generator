dadf7e679c283786134d3827d3df3af681c91a25
jack.loveday95@gmail.com <jack.loveday95@gmail.com>
2021-10-12 23:04:24 -0400

    Created class functions and inquirer input prompts.

--- index.js
+++ index.js
@@ -15,13 +15,173 @@ const inquirer = require('inquirer');
 // Initialize Team Array
 const teamArray = [];
 
-// Add Manager Prompts here
-//
-//
 
-// Add Employee Prompts here
-//
-//
+// Manager Inquirer Prompts
+const addManager = () => {
+    return inquirer.prompt([
+        // Set up Inquirer Prompts
+        {
+            // Manager Name Prompt
+            type: 'input',
+            name: 'name',
+            message: 'Enter the Team Managers Name:',
+            validate: nameInput => {
+                if (nameInput) return true;
+                console.log('Please enter an appropriate name.');
+                return false;
+            }
+        },
+        {
+            // Manager ID Prompt
+            type: 'input',
+            name: 'id',
+            message: 'Enter the Managers ID:',
+            validate: nameInput => {
+                if (isNaN(nameInput)) {
+                    console.log('ID must be an integer.');
+                    return false;
+                }
+                return true;
+            }
+        },
+        {
+            // Manager Email Prompt
+            type: 'input',
+            name: 'email',
+            message: 'Enter the Managers Email:',
+            validate: email => {
+                valid = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email);
+                if (valid) return true;
+                console.log('Enter a valid Email Address.');
+                return false;
+            }
+        },
+        {
+            // Manager Office Number
+            type: 'input',
+            name: 'officeNumber',
+            message: 'Enter the Office Number:',
+            validate: nameInput => {
+                if (isNaN(nameInput)) {
+                    console.log('Office Number must be an integer.');
+                    return false;
+                }
+                return true;
+            }
+        }
+    ])
+        .then(managerInput => {
+            // Then, let's create the Manager from our input
+            const { name, id, email, officeNumber } = managerInput;
+            const manager = new Manager(name, id, email, officeNumber);
+
+            // Add it to our Team Array
+            teamArray.push(manager);
+
+            // Log it for testing purposes
+            console.log(manager);
+        })
+};
+
+
+// Employee Inquirer Prompts
+const addEmployee = () => {
+    return inquirer.prompt([
+        {
+            // Choose the Employee Type/Role
+            type: 'list',
+            name: 'role',
+            message: 'Choose the Employee Role:',
+            choices: ['Engineer', 'Intern']
+        },
+        {
+            // Employee Name Prompt
+            type: 'input',
+            name: 'name',
+            message: 'Enter the Employees Name:',
+            validate: nameInput => {
+                if (nameInput) return true;
+                console.log('Please enter an appropriate name.');
+                return false;
+            }
+        },
+        {
+            // Employee ID Prompt
+            type: 'input',
+            name: 'id',
+            message: 'Enter the Employee ID:',
+            validate: nameInput => {
+                if (isNaN(nameInput)) {
+                    console.log('ID must be an integer.');
+                    return false;
+                }
+                return true;
+            }
+        },
+        {
+            // Employee Email Prompt
+            type: 'input',
+            name: 'email',
+            message: 'Enter the Employees Email:',
+            validate: email => {
+                valid = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email);
+                if (valid) return true;
+                console.log('Enter a valid Email Address.');
+                return false;
+            }
+        },
+        {
+            // Intern's School Prompt
+            type: 'input',
+            name: 'school',
+            message: 'Enter the Interns School Name:',
+            validate: nameInput => {
+                if (nameInput) return true;
+                console.log('Enter a valid School Name.');
+            }
+        },
+        {
+            // Engineer's Github Prompt
+            type: 'input',
+            name: 'github',
+            message: 'Enter the Employees Github Username:',
+            when: (input) => input.role === 'Engineer',
+            validate: nameInput => {
+                if (nameInput) return true;
+                console.log('Enter a valid Github Username.');
+            }
+        },
+        {  
+            // Ask User if they want to add additional Team Members
+            type: 'confirm',
+            name: 'confirmAddEmployee',
+            message: 'Would youb like to add another Team Member?',
+            default: false
+        }
+    ])
+    .then(employeeData => {
+        // Create the Employee from Input
+        let { name, id, email, role, github, school, confirmAddEmployee } = employeeData;
+        let employee; 
+
+        // Check if its either an Engineer, or Intern, and create new
+        if (role === 'Engineer') {
+            employee = new Engineer(name, id, email, github);
+            console.log(employee);
+        } else if (role === 'Intern') {
+            employee = new Intern(name, id, email, school);
+            console.log(employee);
+        }
+
+        // Push to Team Array
+        teamArray.push(employee);
+
+        // If we confirmed adding another...
+        if (confirmAddEmployee) return addEmployee(teamArray);
+        return teamArray;
+    })
+}
+
 
 // Function to write our HTML page
 const writeFile = (data) => {
